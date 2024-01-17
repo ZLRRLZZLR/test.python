a45d09cf7993a3db57eaea9d59221ac17498e6e8
zhou-liurong <19551230598@163.com>
2024-01-17 22:46:47 +0800

    MOOC.1

--- test.2.py
+++ test.2.py
@@ -1,5 +1,14 @@
-count = 0
-for y in range(1,2025):
-    if "2" in str(y):
-        count+=1
-print(count)
+M = input()
+N = input()
+op = input()
+if op=="+":
+    print(eval(M)+eval(N))
+elif op=="-":
+    print(eval(M)+eval(N))
+elif op=="*":
+    print(eval(M)*eval(N))
+else:
+    print(eval(M)/eval(N))
+
+s = input()
+print("{:.2f}".format(eval(s)))
