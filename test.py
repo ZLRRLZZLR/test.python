34fb047da2452715fa55cb25efe9201b42e98168
zhou-liurong <19551230598@163.com>
2024-02-08 19:06:46 +0800

    星号三角形 读入一个整数N，N是奇数，输出由星号字符组成的等边三角形，要求： 第1行1个星号，第2行3个星号，第3行5个星号，依次类推，最后一行共N的星号。

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-count = 0
-for a in range(1,5):
-    for b in range(1,5):
-        for c in range(1,5):
-            if a != b and b != c:
-                print(f'{a}{b}{c}')
-                count+=1
-print(count)
+N = eval(input())
+for i in range(1,N+1,2):
+    print("{: ^{}}".format('*'*i,N))
