aa6e5cd9ff22639c141a9b4139b41e61f7e8f985
zhou-liurong <19551230598@163.com>
2024-02-14 16:30:38 +0800

    无

--- pythonProject/第四周.3、.py
+++
@@ -1,5 +0,0 @@
-S = 0
-for  i in range(1,967):
-    a = (-1) ** (i-1)
-    S += i * a
-print(S)
--- pythonProject/第四周.4.py
+++
@@ -1,6 +0,0 @@
-s = ""
-for i in range(100,1000):
-    t = str(i)
-    if pow(eval(t[0]),3)+pow(eval(t[1]),3)+pow(eval(t[2]),3) ==i:
-        s += "{},".format(i)
-print(s[:-1])
