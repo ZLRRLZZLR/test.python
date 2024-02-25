aad500e15dba938dc9a4a118e48ca3b98be1e034
zhou-liurong <19551230598@163.com>
2024-02-25 21:44:33 +0800

    附件是一个CSV文件，其中每个数据前后存在空格，请对其进行清洗，要求如下： （1）去掉每个数据前后空格，即数据之间仅用逗号(,)分割； （2）清洗后打印输出。

--- pythonProject/test.5.py
+++ pythonProject/test.5.py
@@ -1,21 +1,4 @@
-count = 0
-def prime(m):
-        for i in range(2,m):
-            if m % i == 0:
-                return False
-        return True
-n = int(eval(input()))
-p = int(n)
-p = p+1 if p <n else n
-
-while count < 5:
-    if prime(p):
-        if count < 4:
-            print(p, end =",")
-        else:
-            print(p, end ="")
-        count += 1
-    p += 1
-
-
-
+f = open("data.csv")
+s = f.read()
+s = s.replace(" ","")
+print(s)
