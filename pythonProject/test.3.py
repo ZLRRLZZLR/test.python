ed2a60d89c6dc211535f1c564c9cbf68d88939b8
zhou-liurong <19551230598@163.com>
2024-02-25 21:40:17 +0800

    附件是一个CSV文件，请将每行按照列逆序排列后输出，不改变各元素格式（如周围空格布局等）。

--- pythonProject/test.3.py
+++ pythonProject/test.3.py
@@ -1,5 +1,7 @@
-def cmul(a,*b):
-    for i in b:
-        a *= i
-    return a
-print(eval("cmul({})".format(input())))
+f = open("latex.log")
+ls = f.readlines()
+s = set(ls)
+for i in s:
+    ls.remove(i)
+t = set(ls)
+print("共{}独特行".format(len(s)-len((t))))
