dd1d62c3bf4bc3a7a55b9e4a55db8de0aaa98e87
zhou-liurong <19551230598@163.com>
2024-05-19 08:22:07 +0800

    python123_test2

---
+++ pythonProject/two_week.py
@@ -0,0 +1,43 @@
+# #RectDraw.py
+# import turtle as t
+# t.pensize(2)
+# for i in range(4):
+#     t.fd(150)
+#     t.left(90)
+
+# #HexagonDraw.py
+# import turtle as t
+# t.pensize(2)
+# for i in range(6):
+#     t.fd(150)
+#     t.left(60)
+
+# #TwoRoundDraw.py
+# import turtle as t
+# t.pensize(2)
+# for i in range(9):
+#     t.fd(150)
+#     t.left(80)  #720/9
+
+# #WindWheel.py
+# import turtle as t
+# t.pensize(2)
+# for i in range(4):
+#     t.seth(90*i)
+#     t.fd(150)
+#     t.right(90)
+#     t.circle(-150, 45)
+#     t.goto(0,0)
+
+# #OctagonDraw.py
+# import turtle as t
+# t.pensize(2)
+# for i in range(8):
+#     t.fd(100)
+#     t.left(45)
+
+# import turtle as t
+# t.pensize(2)
+# for i in range(8):
+#     t.fd(150)
+#     t.left(135)
